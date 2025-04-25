362c37e9e52fe2151434e70c52b306f86a0909a2
linnabraham <linn.official@gmail.com>
2025-04-25 16:33:17 +0530

    Add documentation for module

---
+++ astro_utils/__init__.py
@@ -0,0 +1,18 @@
+"""
+Astro Utils
+===========
+
+A collection of utilities for astrophysics data processing and visualization.
+
+Available modules:
+----------------
+aia : Functions for SDO/AIA image processing
+flare : Solar flare data downloading and analysis
+general : General FITS file handling utilities
+
+Example usage:
+-------------
+>>> from astro_utils import read_fits_single
+>>> from astro_utils.aia import plot_aia_image
+>>> from astro_utils.flare import fetch_goes_data
+"""
